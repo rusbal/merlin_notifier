01bb6197558f6c3fed630b20058c4c3b793db165
Raymond Usbal <raymond@philippinedev.com>
2024-10-10 14:23:21 +0800

    postgres trigger migration

--- db/functions/products_trigger_v01.sql
+++
@@ -1,4 +0,0 @@
-CREATE TRIGGER products_notifier
-    AFTER INSERT OR UPDATE OR DELETE ON products
-    FOR EACH ROW
-    EXECUTE PROCEDURE products_notifier();
--- db/migrate/20241010061326_create_function_products_trigger.rb
+++
@@ -1,5 +0,0 @@
-class CreateFunctionProductsTrigger < ActiveRecord::Migration[7.0]
-  def change
-    create_function :products_trigger
-  end
-end
